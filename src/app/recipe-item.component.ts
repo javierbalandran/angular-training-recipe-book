5495227e36763336ae87b8f7c5788dc04c5906d8
Javier Balandran <javierbalandran@gmail.com>
2020-07-19 21:57:22 -0700

    Learning property and event binding

--- src/app/recipe-item.component.ts
+++ src/app/recipe-item.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
     styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent{
+    name = "PB&J";
 
+    onDelete() {
+        console.log("delete");
+    }
 }

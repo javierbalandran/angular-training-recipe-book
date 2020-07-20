e8f76b28898d4dc59b33d1f62641b42a06eaad16
Javier Balandran <javierbalandran@gmail.com>
2020-07-19 21:43:03 -0700

    Added new RecipeItemComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { RecipeItemComponent } from './recipe-item.component'
 
 @NgModule({
     imports: [
         BrowserModule
     ],
     declarations: [
-        AppComponent
+        AppComponent,
+        RecipeItemComponent
     ],
     bootstrap: [
         AppComponent
---
+++ src/app/recipe-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'rb-recipe-item',
+    templateUrl: './recipe-item.component.html',
+    styleUrls: ['./recipe-item.component.css']
+})
+export class RecipeItemComponent{
+
+}

db6b31df3a29498b1f7e4fc96d56ac09ed2d5e44
Javier Balandran <javierbalandran@gmail.com>
2020-08-09 14:17:56 -0700

    Registering routing in the app module

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { RecipeItemFormComponent } from './recipe-item-form.component';
 import { RecipeItemListComponent } from './recipe-item-list.component';
 
@@ -7,3 +7,5 @@ const appRoutes: Routes = [
     { path: ':medium', component: RecipeItemListComponent },
     { path: '', redirectTo: 'all', pathMatch: 'full' }
 ];
+
+export const routing = RouterModule.forRoot(appRoutes);
